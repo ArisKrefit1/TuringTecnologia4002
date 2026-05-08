1560fdc3194d1e0af15096553f020cd411a0591f
Aris <canal2ariscraft65@gmail.com>
2026-05-07 23:38:13 -0300

    Delete ParticipanteRepository.java

--- ParticipanteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.repository;
-
-import com.api.model.Participante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParticipanteRepository extends JpaRepository<Participante, Long> {
-}
